44f8dbd98759ef2ef328979060df715161cc7841
SalmaAbied <114472834+SalmaAbied@users.noreply.github.com>
2024-05-16 12:23:57 +0200

    .

--- qastan-sa/src/pages/Home.tsx
+++ qastan-sa/src/pages/Home.tsx
@@ -4,7 +4,7 @@ const Home: React.FC = () => {
   return (
     <div className="p-4">
       <h1 className="text-2xl font-bold">Home Page</h1>
-      <p>Welcome to the home page!</p>
+      <p>home</p>
     </div>
   );
 }
