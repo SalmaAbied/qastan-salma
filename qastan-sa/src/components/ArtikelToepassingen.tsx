1f680d3fd3693f1bb3b7759f6208981218c4dfe2
SalmaAbied <114472834+SalmaAbied@users.noreply.github.com>
2024-06-05 15:30:24 +0200

    json id toegevoegd

---
+++ qastan-sa/src/components/ArtikelToepassingen.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ArtikelToepassingen() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ArtikelToepassingen
--- qastan-sa/src/pages/ToepassingenDetail.tsx
+++ qastan-sa/src/pages/ToepassingenDetail.tsx
@@ -11,11 +11,26 @@ const ToepassingenDetail: React.FC = () => {
   }
 
   return (
-    <div>
-      <h1>{post.title}</h1>
-      <img src={post.imageUrl} alt={`image about ${post.title}`} />
-      <p>{post.description}</p>
-    </div>
+    <>
+      <div className="container mx-auto py-20">
+        {post.title}
+        <div className="flex flex-wrap items-center justify-between p-8">
+          <div className="w-full md:w-1/2 md:pr-8 mb-8 md:mb-0">
+            <h1 className="text-3xl font-bold mb-4">{post.probleem.title}</h1>
+            <p className="text-lg text-gray-700 mb-6">{post.probleem.description}</p>
+            <h2 className="text-3xl font-bold mb-2">{post.oplossing.title}</h2>
+            <p className="text-lg text-gray-700 mb-6">{post.oplossing.description}</p>
+            <a href={post.href} className="inline-block bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600">
+              {post.buttontext}
+            </a>
+          </div>
+          <div>{post.uitleg}</div>
+          <div className="w-full md:w-1/2">
+            <img src={post.imageURL} alt={`image about ${post.probleem.title}`} className="w-full rounded-lg" />
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
