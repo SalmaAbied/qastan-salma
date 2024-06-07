4edd0f48ebde28f6a7a2a7636eb898d962909762
SalmaAbied <114472834+SalmaAbied@users.noreply.github.com>
2024-06-07 17:11:32 +0200

    bezig met de detail page van de toepassingen

--- qastan-sa/src/pages/ToepassingenDetail.tsx
+++ qastan-sa/src/pages/ToepassingenDetail.tsx
@@ -11,26 +11,40 @@ const ToepassingenDetail: React.FC = () => {
   }
 
   return (
-    <>
-      <div className="container mx-auto py-20">
-        {post.title}
-        <div className="flex flex-wrap items-center justify-between p-8">
+    <div className="container mx-auto py-20">
+      <div className="bg-white shadow-md rounded-lg overflow-hidden">
+        <div className="flex flex-wrap items-center p-8">
           <div className="w-full md:w-1/2 md:pr-8 mb-8 md:mb-0">
-            <h1 className="text-3xl font-bold mb-4">{post.probleem.title}</h1>
-            <p className="text-lg text-gray-700 mb-6">{post.probleem.description}</p>
-            <h2 className="text-3xl font-bold mb-2">{post.oplossing.title}</h2>
-            <p className="text-lg text-gray-700 mb-6">{post.oplossing.description}</p>
-            <a href={post.href} className="inline-block bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600">
+            <h1 className="text-4xl font-bold mb-6 text-darkblue">{post.title}</h1>
+            <div className="mb-8">
+              <h2 className="text-3xl font-semibold mb-4 text-gray-800">{post.probleem.title}</h2>
+              <p className="text-lg text-gray-700">{post.probleem.description}</p>
+            </div>
+            <div className="mb-8">
+              <h2 className="text-3xl font-semibold mb-4 text-gray-800">{post.oplossing.title}</h2>
+              <p className="text-lg text-gray-700">{post.oplossing.description}</p>
+            </div>
+            <a
+              href={post.href}
+              className="inline-block bg-darkorange text-white py-2 px-4 rounded hover:bg-lightblue transition duration-300"
+            >
               {post.buttontext}
             </a>
           </div>
-          <div>{post.uitleg}</div>
           <div className="w-full md:w-1/2">
-            <img src={post.imageURL} alt={`image about ${post.probleem.title}`} className="w-full rounded-lg" />
+            <img
+              src={post.imageURL}
+              alt={`image about ${post.probleem.title}`}
+              className="w-full rounded-lg shadow-lg"
+            />
           </div>
         </div>
+        <div className="p-8 border-t border-gray-200">
+          <h2 className="text-2xl font-bold mb-4 text-gray-800">Uitleg</h2>
+          <p className="text-lg text-gray-700">{post.uitleg}</p>
+        </div>
       </div>
-    </>
+    </div>
   );
 };
 
